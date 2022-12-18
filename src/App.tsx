e5a9f97f334e181c38e4cd3fe4427df5ca6315c6
Hardy Lutula <hardylutula@gmail.com>
2022-12-18 21:23:01 +0200

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import {useState} from 'react'
 import words from './wordList.json'
 
 const App = () => {
-  const [wordToGuess, setWordToGuess] = useState(() => {
+  const [wordToGuess, setWordToGuess] = useState<string>(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
 
   console.log(wordToGuess)
   return <div>App</div>
